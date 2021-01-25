9c26c33a468805ad5f76a65266f733bac76541d6
finbyz <info@finbyz.com>
2021-01-25 08:22:44 +0000

    Activity Analysis Changes

--- finbyz_dashboard/finbyz_dashboard/report/activity_analysis/activity_analysis.js
+++ finbyz_dashboard/finbyz_dashboard/report/activity_analysis/activity_analysis.js
@@ -27,17 +27,11 @@ frappe.query_reports["Activity Analysis"] = {
 			"options": "User"
 		},
 		{
-			fieldname:"timespan",
-			label: __("Timespan"),
-			fieldtype: "Select",
-			width: "80",
-			options:"last week\nlast month\nlast quarter\nlast 6 months\nlast year\ntoday\nthis week\nthis month\nthis quarter\nthis year\nthis fiscal year\nnext week\nnext month\nnext quarter\nnext 6 months\nnext year"
-		},
-		{
-			fieldname:"use_timespan",
-			label: __("Use Timespan"),
-			fieldtype: "Check",
-			default:0
-		},
+			"fieldname":"timespan",
+			"label": __("Timespan"),
+			"fieldtype": "Select",
+			"width": "80",
+			"options":"\nlast week\nlast month\nlast quarter\nlast 6 months\nlast year\ntoday\nthis week\nthis month\nthis quarter\nthis year\nthis fiscal year\nnext week\nnext month\nnext quarter\nnext 6 months\nnext year"
+		}
 	]
 }
